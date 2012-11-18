8f5d87335f3b73b154bdbe741f929e2dd91eb086
prjemian <prjemian@57561bab-14d3-4231-89cc-d4d07b544de1>
2012-11-18 00:32:59 +0000

    better display of math equations

--- canSAS2012/docs/source/conf.py
+++ canSAS2012/docs/source/conf.py
@@ -25,7 +25,11 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.viewcode']
+#extensions = ['sphinx.ext.pngmath',]
+extensions = ['sphinx.ext.mathjax',]
+extensions.append('sphinx.ext.autodoc')
+extensions.append('sphinx.ext.todo')
+extensions.append('sphinx.ext.viewcode')
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']

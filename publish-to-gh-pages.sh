ed619221e02022835400e6b708093e1fa6cce2bc
Tobias Richter <tobias.richter@esss.se>
2015-12-05 10:58:53 +0100

    have travis infrastructure ready
    css and other items from _statis not displayed at gh-pages
    no idea

--- publish-to-gh-pages.sh
+++ publish-to-gh-pages.sh
@@ -24,9 +24,10 @@ git config user.email "tobias.richter@esss.se"
 git add .
 git commit -m "Deploy to GitHub Pages"
 
+echo pushing to github
 # Force push from the current repo's master branch to the remote
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-##git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1 && echo pushed with no error 
+echo end of script
